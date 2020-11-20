3bcd1d5437d6cc7f7e0ed01b48f25d23e80f906b
ddojai <favianred@gmail.com>
2020-11-20 12:23:32 +0900

    Add application.yml to resource
    - Apply jpa auditing for datetime column

--- src/main/java/com/ddojai/github/spinkle/Application.java
+++ src/main/java/com/ddojai/github/spinkle/Application.java
@@ -2,7 +2,9 @@ package com.ddojai.github.spinkle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
